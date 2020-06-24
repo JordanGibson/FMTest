c07fc91c69b99a3f4e0d8477e7b0e2b61d64a9f5
JordanGibson <jordang@kainos.com>
2020-06-24 15:21:13 +0100

    Update migrations

--- FMTest/Migrations/202006231445_CreateSecurityAddress.cs
+++ FMTest/Migrations/202006231445_CreateSecurityAddress.cs
@@ -19,6 +19,34 @@ namespace FMTest.Migrations
 
             Alter.Table("user").InSchema("security").AddColumn("address_id").AsInt32()
                 .ForeignKey("fk_security_user_address", "security", "address", "id");
+
+            Execute.Sql(@"CREATE OR REPLACE FUNCTION security.user_to_json (name varchar, role_name varchar, street varchar, town varchar) RETURNS jsonb AS $$
+	                        BEGIN
+ 	                        RETURN json_build_object(
+ 			                        'name', name,
+			                        'role', json_build_object(
+				                        'name', role_name
+			                        ),
+			                        'address', json_build_object(
+				                        'street', street,
+				                        'town', town
+			                        )
+ 		                        );
+	                        END;
+                        $$ LANGUAGE plpgsql;
+
+                        CREATE OR REPLACE VIEW security.user_json AS
+                        SELECT
+	                        security.user_to_json(
+		                        u.first_name || ' ' || u.last_name,
+		                        r.name,
+		                        a.street,
+		                        a.town)
+                        FROM security.user u
+                        JOIN security.role r
+	                        ON r.id = u.role_id
+                        LEFT JOIN security.address a
+	                        ON a.id = u.address_id;");
         }
 
         public override void Down()
